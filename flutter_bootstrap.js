8a8bc4c6db71fec9c109be7c24f892d781d436e6
innasb <95342813+innasb@users.noreply.github.com>
2026-05-01 23:36:24 +0000

    Deploying to gh-pages from @ innasb/movieapp@b28402e5d3df4ef78d6e51fd0d8d20be8173b63d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1018920005" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2053095171" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
